d2a825c52ce6555bb7eea3f91feb3fb6f38855c6
SolosoftNL <solosoft.nl@gmail.com>
2016-02-15 19:56:58 +0100

    Init

---
+++ Acme.Common/OperationResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult<TResult, TMessage>
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(TResult success, TMessage message) : this()
+        {
+            this.Result = success;
+            this.Message = message;
+        }
+
+        public TResult Result { get; set; }
+        public TMessage Message { get; set; }
+    }
+}
---
+++ Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorRepositoryTests
+    {
+        [TestMethod()]
+        public void RetrieveValueTestWithInt()
+        {
+            //Arrange
+            var repo = new VendorRepository();
+            var expected = 32;
+
+            //Act
+            var actual = repo.RetrieveValue<int>("Select  ...", 32);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueTestWithString()
+        {
+            //Arrange
+            var repo = new VendorRepository();
+            var expected = "* From People Where Name == 'Ronald'";
+
+            //Act
+            var actual = repo.RetrieveValue<string>("Select  ...", "* From People Where Name == 'Ronald'");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
